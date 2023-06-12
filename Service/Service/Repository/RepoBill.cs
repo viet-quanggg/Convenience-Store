0d342bcaa37763dc7f4fc43610b168e5a48c6588
viet-quanggg <nguyenvietquang099@gmail.com>
2023-06-12 07:57:47 +0700

    cap nhat repo

--- Service/Service/Repository/RepoBill.cs
+++ Service/Service/Repository/RepoBill.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Service.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Service.Repository
 {
-    internal class RepoBill
+    internal class RepoBill : RepoBase<Bill>
     {
     }
 }
