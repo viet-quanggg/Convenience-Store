aee4b345a8b50b036aac992fadf44cba4ef263f0
caothang7a7 <caothang7a7@gmail.com>
2023-06-12 22:00:56 +0700

    design(home page, account setting, login form)

---
+++ Convenience Store/AccountSetting.Designer.cs
@@ -0,0 +1,415 @@
+﻿namespace Convenience_Store
+{
+    partial class AccountSetting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            panel2 = new Panel();
+            label3 = new Label();
+            btnExit = new Button();
+            label6 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            txtPassword = new TextBox();
+            dtpDOB = new DateTimePicker();
+            cbId = new ComboBox();
+            cbRole = new ComboBox();
+            txtName1 = new TextBox();
+            txtAddress = new TextBox();
+            txtPhone = new TextBox();
+            panel3 = new Panel();
+            label13 = new Label();
+            panel4 = new Panel();
+            label14 = new Label();
+            txtRole = new TextBox();
+            txtName2 = new TextBox();
+            txtId = new TextBox();
+            label12 = new Label();
+            btnLogout = new Button();
+            btnSave = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(-27, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1149, 105);
+            panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label1.BackColor = SystemColors.ActiveCaptionText;
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Location = new Point(-15, 93);
+            label1.Name = "label1";
+            label1.Size = new Size(1009, 10);
+            label1.TabIndex = 18;
+            label1.Text = "label1";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(292, 8);
+            label2.Name = "label2";
+            label2.Size = new Size(358, 60);
+            label2.TabIndex = 19;
+            label2.Text = "Account Setting";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(btnExit);
+            panel2.Location = new Point(2, 597);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(920, 77);
+            panel2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(797, 41);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 20);
+            label3.TabIndex = 20;
+            label3.Text = "Setting Page";
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(54, 33);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(94, 29);
+            btnExit.TabIndex = 19;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label6.BackColor = SystemColors.ActiveCaptionText;
+            label6.BorderStyle = BorderStyle.FixedSingle;
+            label6.Location = new Point(1, 593);
+            label6.Name = "label6";
+            label6.Size = new Size(928, 10);
+            label6.TabIndex = 18;
+            label6.Text = "label6";
+            label6.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(7, 10);
+            label4.Name = "label4";
+            label4.Size = new Size(29, 20);
+            label4.TabIndex = 4;
+            label4.Text = "Id :";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(15, 63);
+            label5.Name = "label5";
+            label5.Size = new Size(56, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Name :";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(15, 107);
+            label7.Name = "label7";
+            label7.Size = new Size(77, 20);
+            label7.TabIndex = 4;
+            label7.Text = "Password :";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(15, 159);
+            label8.Name = "label8";
+            label8.Size = new Size(101, 20);
+            label8.TabIndex = 4;
+            label8.Text = "Date of Birth :";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(15, 208);
+            label9.Name = "label9";
+            label9.Size = new Size(57, 20);
+            label9.TabIndex = 4;
+            label9.Text = "Phone :";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(15, 256);
+            label10.Name = "label10";
+            label10.Size = new Size(46, 20);
+            label10.TabIndex = 4;
+            label10.Text = "Role :";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(15, 303);
+            label11.Name = "label11";
+            label11.Size = new Size(69, 20);
+            label11.TabIndex = 4;
+            label11.Text = "Address :";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(122, 107);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(125, 27);
+            txtPassword.TabIndex = 5;
+            // 
+            // dtpDOB
+            // 
+            dtpDOB.Location = new Point(122, 154);
+            dtpDOB.Name = "dtpDOB";
+            dtpDOB.Size = new Size(250, 27);
+            dtpDOB.TabIndex = 6;
+            // 
+            // cbId
+            // 
+            cbId.FormattingEnabled = true;
+            cbId.Location = new Point(122, 15);
+            cbId.Name = "cbId";
+            cbId.Size = new Size(151, 28);
+            cbId.TabIndex = 7;
+            // 
+            // cbRole
+            // 
+            cbRole.FormattingEnabled = true;
+            cbRole.Location = new Point(122, 253);
+            cbRole.Name = "cbRole";
+            cbRole.Size = new Size(151, 28);
+            cbRole.TabIndex = 7;
+            // 
+            // txtName1
+            // 
+            txtName1.Location = new Point(122, 63);
+            txtName1.Name = "txtName1";
+            txtName1.Size = new Size(125, 27);
+            txtName1.TabIndex = 5;
+            // 
+            // txtAddress
+            // 
+            txtAddress.Location = new Point(122, 303);
+            txtAddress.Name = "txtAddress";
+            txtAddress.Size = new Size(125, 27);
+            txtAddress.TabIndex = 5;
+            // 
+            // txtPhone
+            // 
+            txtPhone.Location = new Point(122, 202);
+            txtPhone.Name = "txtPhone";
+            txtPhone.Size = new Size(125, 27);
+            txtPhone.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(label13);
+            panel3.Controls.Add(cbRole);
+            panel3.Controls.Add(cbId);
+            panel3.Controls.Add(dtpDOB);
+            panel3.Controls.Add(txtName1);
+            panel3.Controls.Add(txtPhone);
+            panel3.Controls.Add(txtAddress);
+            panel3.Controls.Add(txtPassword);
+            panel3.Controls.Add(label11);
+            panel3.Controls.Add(label10);
+            panel3.Controls.Add(label9);
+            panel3.Controls.Add(label8);
+            panel3.Controls.Add(label7);
+            panel3.Controls.Add(label5);
+            panel3.Location = new Point(430, 107);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(487, 344);
+            panel3.TabIndex = 8;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(15, 18);
+            label13.Name = "label13";
+            label13.Size = new Size(29, 20);
+            label13.TabIndex = 8;
+            label13.Text = "Id :";
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(label14);
+            panel4.Controls.Add(txtRole);
+            panel4.Controls.Add(txtName2);
+            panel4.Controls.Add(txtId);
+            panel4.Controls.Add(label12);
+            panel4.Controls.Add(label4);
+            panel4.Location = new Point(5, 112);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(421, 201);
+            panel4.TabIndex = 9;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(7, 109);
+            label14.Name = "label14";
+            label14.Size = new Size(46, 20);
+            label14.TabIndex = 8;
+            label14.Text = "Role :";
+            // 
+            // txtRole
+            // 
+            txtRole.Location = new Point(114, 106);
+            txtRole.Name = "txtRole";
+            txtRole.Size = new Size(125, 27);
+            txtRole.TabIndex = 7;
+            // 
+            // txtName2
+            // 
+            txtName2.Location = new Point(114, 55);
+            txtName2.Name = "txtName2";
+            txtName2.Size = new Size(125, 27);
+            txtName2.TabIndex = 7;
+            // 
+            // txtId
+            // 
+            txtId.Enabled = false;
+            txtId.Location = new Point(114, 3);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(125, 27);
+            txtId.TabIndex = 7;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(7, 58);
+            label12.Name = "label12";
+            label12.Size = new Size(56, 20);
+            label12.TabIndex = 6;
+            label12.Text = "Name :";
+            // 
+            // btnLogout
+            // 
+            btnLogout.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            btnLogout.Location = new Point(586, 501);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(101, 39);
+            btnLogout.TabIndex = 9;
+            btnLogout.Text = "Logout";
+            btnLogout.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            btnSave.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            btnSave.Location = new Point(790, 501);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(101, 39);
+            btnSave.TabIndex = 10;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            // 
+            // AccountSetting
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(920, 673);
+            Controls.Add(btnSave);
+            Controls.Add(btnLogout);
+            Controls.Add(label6);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "AccountSetting";
+            Text = "AccountSetting";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label2;
+        private Label label1;
+        private Panel panel2;
+        private Label label3;
+        private Button btnExit;
+        private Label label6;
+        private Label label4;
+        private Label label5;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private TextBox txtPassword;
+        private DateTimePicker dtpDOB;
+        private ComboBox cbId;
+        private ComboBox cbRole;
+        private TextBox txtName1;
+        private TextBox txtAddress;
+        private TextBox txtPhone;
+        private Panel panel3;
+        private Label label13;
+        private Panel panel4;
+        private Label label14;
+        private TextBox txtRole;
+        private TextBox txtName2;
+        private TextBox txtId;
+        private Label label12;
+        private Button btnLogout;
+        private Button btnSave;
+    }
+}
