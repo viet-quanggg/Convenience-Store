8ba07eb5eb583bce9a32a623d34850ee438fe42b
minh2003vt <109030985+minh2003vt@users.noreply.github.com>
2023-06-16 07:24:51 +0700

    layout merchandise,order

---
+++ Convenience Store/MechandiseList.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace MechandiseList
+{
+    partial class MechandiseList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label7 = new Label();
+            panel2 = new Panel();
+            label4 = new Label();
+            button1 = new Button();
+            panel1 = new Panel();
+            label6 = new Label();
+            label3 = new Label();
+            dataGridView1 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            label1 = new Label();
+            btnRemove = new Button();
+            btnFinish = new Button();
+            btnAdd = new Button();
+            textBox1 = new TextBox();
+            btnSearch = new Button();
+            panel2.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            SuspendLayout();
+            // 
+            // label7
+            // 
+            label7.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label7.BackColor = SystemColors.ActiveCaptionText;
+            label7.BorderStyle = BorderStyle.FixedSingle;
+            label7.Location = new Point(1, 589);
+            label7.Margin = new Padding(4, 0, 4, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(1134, 12);
+            label7.TabIndex = 23;
+            label7.Text = "label7";
+            label7.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(button1);
+            panel2.Location = new Point(8, 605);
+            panel2.Margin = new Padding(4);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1140, 155);
+            panel2.TabIndex = 20;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(990, 11);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(131, 32);
+            label4.TabIndex = 1;
+            label4.Text = "Login Page";
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.BorderSize = 2;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(12, 6);
+            button1.Margin = new Padding(4);
+            button1.Name = "button1";
+            button1.Size = new Size(118, 36);
+            button1.TabIndex = 0;
+            button1.Text = "Exit";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label3);
+            panel1.ForeColor = SystemColors.MenuText;
+            panel1.Location = new Point(1, 13);
+            panel1.Margin = new Padding(4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1148, 144);
+            panel1.TabIndex = 19;
+            // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label6.BackColor = SystemColors.ActiveCaptionText;
+            label6.BorderStyle = BorderStyle.FixedSingle;
+            label6.Location = new Point(-11, 131);
+            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(2106, 12);
+            label6.TabIndex = 17;
+            label6.Text = "label6";
+            label6.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 48F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(183, -4);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(763, 128);
+            label3.TabIndex = 0;
+            label3.Text = "Merchandise List";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(502, 218);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.RowTemplate.Height = 33;
+            dataGridView1.Size = new Size(583, 276);
+            dataGridView1.TabIndex = 24;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(55, 218);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersWidth = 62;
+            dataGridView2.RowTemplate.Height = 33;
+            dataGridView2.Size = new Size(362, 276);
+            dataGridView2.TabIndex = 25;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(72, 156);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(316, 54);
+            label1.TabIndex = 18;
+            label1.Text = "Currently Added";
+            // 
+            // btnRemove
+            // 
+            btnRemove.FlatAppearance.BorderSize = 2;
+            btnRemove.FlatStyle = FlatStyle.Flat;
+            btnRemove.Location = new Point(123, 514);
+            btnRemove.Margin = new Padding(4);
+            btnRemove.Name = "btnRemove";
+            btnRemove.Size = new Size(190, 56);
+            btnRemove.TabIndex = 2;
+            btnRemove.Text = "Remove";
+            btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // btnFinish
+            // 
+            btnFinish.FlatAppearance.BorderSize = 2;
+            btnFinish.FlatStyle = FlatStyle.Flat;
+            btnFinish.Location = new Point(888, 514);
+            btnFinish.Margin = new Padding(4);
+            btnFinish.Name = "btnFinish";
+            btnFinish.Size = new Size(190, 56);
+            btnFinish.TabIndex = 27;
+            btnFinish.Text = "Finish";
+            btnFinish.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            btnAdd.FlatAppearance.BorderSize = 2;
+            btnAdd.FlatStyle = FlatStyle.Flat;
+            btnAdd.Location = new Point(676, 514);
+            btnAdd.Margin = new Padding(4);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(190, 56);
+            btnAdd.TabIndex = 28;
+            btnAdd.Text = "Add Into Order";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(502, 164);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Search Box";
+            textBox1.Size = new Size(445, 31);
+            textBox1.TabIndex = 29;
+            // 
+            // btnSearch
+            // 
+            btnSearch.FlatAppearance.BorderSize = 2;
+            btnSearch.FlatStyle = FlatStyle.Flat;
+            btnSearch.Location = new Point(973, 160);
+            btnSearch.Margin = new Padding(4);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(112, 39);
+            btnSearch.TabIndex = 30;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // MechandiseList
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1136, 711);
+            Controls.Add(btnSearch);
+            Controls.Add(textBox1);
+            Controls.Add(btnAdd);
+            Controls.Add(btnFinish);
+            Controls.Add(btnRemove);
+            Controls.Add(label1);
+            Controls.Add(dataGridView2);
+            Controls.Add(dataGridView1);
+            Controls.Add(label7);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "MechandiseList";
+            Text = "MechandiseList";
+            Load += MechandiseList_Load;
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label7;
+        private Panel panel2;
+        private Label label4;
+        private Button button1;
+        private Panel panel1;
+        private Label label6;
+        private Label label3;
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
+        private Label label1;
+        private Button btnRemove;
+        private Button btnFinish;
+        private Button btnAdd;
+        private TextBox textBox1;
+        private Button btnSearch;
+    }
+}
---
+++ Convenience Store/Order.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace MechandiseList
+{
+    partial class Order
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            label4 = new Label();
+            panel1 = new Panel();
+            label6 = new Label();
+            label1 = new Label();
+            panel2 = new Panel();
+            label2 = new Label();
+            btnExit = new Button();
+            label3 = new Label();
+            txtTotal = new TextBox();
+            label5 = new Label();
+            btnExport = new Button();
+            btnFinish = new Button();
+            btnEdit = new Button();
+            btnDelete = new Button();
+            btnMerchandise = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(563, 175);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.RowTemplate.Height = 33;
+            dataGridView1.Size = new Size(506, 222);
+            dataGridView1.TabIndex = 13;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(118, 175);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersWidth = 62;
+            dataGridView2.RowTemplate.Height = 33;
+            dataGridView2.Size = new Size(252, 211);
+            dataGridView2.TabIndex = 14;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(563, 432);
+            label4.Name = "label4";
+            label4.Size = new Size(95, 45);
+            label4.TabIndex = 3;
+            label4.Text = "Total:";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label1);
+            panel1.ForeColor = SystemColors.MenuText;
+            panel1.Location = new Point(4, 13);
+            panel1.Margin = new Padding(4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1148, 144);
+            panel1.TabIndex = 20;
+            // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label6.BackColor = SystemColors.ActiveCaptionText;
+            label6.BorderStyle = BorderStyle.FixedSingle;
+            label6.Location = new Point(-11, 131);
+            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(3054, 12);
+            label6.TabIndex = 17;
+            label6.Text = "label6";
+            label6.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 48F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(417, 3);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(299, 128);
+            label1.TabIndex = 0;
+            label1.Text = "Order";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(btnExit);
+            panel2.Location = new Point(4, 519);
+            panel2.Margin = new Padding(4);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1140, 155);
+            panel2.TabIndex = 24;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(990, 11);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(75, 32);
+            label2.TabIndex = 1;
+            label2.Text = "Order";
+            // 
+            // btnExit
+            // 
+            btnExit.FlatAppearance.BorderSize = 2;
+            btnExit.FlatStyle = FlatStyle.Flat;
+            btnExit.Location = new Point(12, 6);
+            btnExit.Margin = new Padding(4);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(118, 36);
+            btnExit.TabIndex = 0;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label3.BackColor = SystemColors.ActiveCaptionText;
+            label3.BorderStyle = BorderStyle.FixedSingle;
+            label3.Location = new Point(-908, 503);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(3051, 12);
+            label3.TabIndex = 18;
+            label3.Text = "label3";
+            label3.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // txtTotal
+            // 
+            txtTotal.BackColor = SystemColors.Menu;
+            txtTotal.BorderStyle = BorderStyle.None;
+            txtTotal.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            txtTotal.Location = new Point(645, 437);
+            txtTotal.Multiline = true;
+            txtTotal.Name = "txtTotal";
+            txtTotal.Size = new Size(75, 34);
+            txtTotal.TabIndex = 25;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(712, 432);
+            label5.Name = "label5";
+            label5.Size = new Size(37, 45);
+            label5.TabIndex = 26;
+            label5.Text = "$";
+            // 
+            // btnExport
+            // 
+            btnExport.FlatAppearance.BorderSize = 2;
+            btnExport.FlatStyle = FlatStyle.Flat;
+            btnExport.Location = new Point(756, 412);
+            btnExport.Margin = new Padding(4);
+            btnExport.Name = "btnExport";
+            btnExport.Size = new Size(146, 74);
+            btnExport.TabIndex = 2;
+            btnExport.Text = "Export Bill";
+            btnExport.UseVisualStyleBackColor = true;
+            // 
+            // btnFinish
+            // 
+            btnFinish.FlatAppearance.BorderSize = 2;
+            btnFinish.FlatStyle = FlatStyle.Flat;
+            btnFinish.Location = new Point(923, 412);
+            btnFinish.Margin = new Padding(4);
+            btnFinish.Name = "btnFinish";
+            btnFinish.Size = new Size(146, 74);
+            btnFinish.TabIndex = 27;
+            btnFinish.Text = "Finish";
+            btnFinish.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            btnEdit.FlatAppearance.BorderSize = 2;
+            btnEdit.FlatStyle = FlatStyle.Flat;
+            btnEdit.Location = new Point(16, 403);
+            btnEdit.Margin = new Padding(4);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(146, 74);
+            btnEdit.TabIndex = 28;
+            btnEdit.Text = "Edit";
+            btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.FlatAppearance.BorderSize = 2;
+            btnDelete.FlatStyle = FlatStyle.Flat;
+            btnDelete.Location = new Point(186, 403);
+            btnDelete.Margin = new Padding(4);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(146, 74);
+            btnDelete.TabIndex = 29;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnMerchandise
+            // 
+            btnMerchandise.FlatAppearance.BorderSize = 2;
+            btnMerchandise.FlatStyle = FlatStyle.Flat;
+            btnMerchandise.Location = new Point(351, 403);
+            btnMerchandise.Margin = new Padding(4);
+            btnMerchandise.Name = "btnMerchandise";
+            btnMerchandise.Size = new Size(146, 74);
+            btnMerchandise.TabIndex = 30;
+            btnMerchandise.Text = "Merchandise";
+            btnMerchandise.UseVisualStyleBackColor = true;
+            // 
+            // Order
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1145, 591);
+            Controls.Add(btnMerchandise);
+            Controls.Add(btnDelete);
+            Controls.Add(btnEdit);
+            Controls.Add(btnFinish);
+            Controls.Add(btnExport);
+            Controls.Add(label5);
+            Controls.Add(txtTotal);
+            Controls.Add(label3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(label4);
+            Controls.Add(dataGridView2);
+            Controls.Add(dataGridView1);
+            Name = "Order";
+            Text = "Order";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
+        private Label label4;
+        private Panel panel1;
+        private Label label6;
+        private Label label1;
+        private Panel panel2;
+        private Label label2;
+        private Button btnExit;
+        private Label label3;
+        private TextBox txtTotal;
+        private Label label5;
+        private Button btnExport;
+        private Button btnFinish;
+        private Button btnEdit;
+        private Button btnDelete;
+        private Button btnMerchandise;
+    }
+}
